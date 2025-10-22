0bd4ab28bf8e23694d211947594221ecdb094676
ekosachev <egorkosachev@vk.com>
2025-10-23 02:20:40 +0300

    [FIX][#6] removed unrelated changes

--- back/internal/models/event.go
+++ back/internal/models/event.go
@@ -4,25 +4,6 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-<<<<<<< HEAD
-)
-
-type Event struct {
-	Id          uuid.UUID              `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
-	CalendarId  uuid.UUID              `gorm:"type:uuid;not null;on delete cascade index:idx_events_calendar"`
-	Title       string                 `gorm:"type:text;not null"`
-	Description string                 `gorm:"type:text"`
-	Location    string                 `gorm:"type:text"`
-	StartAt     time.Time              `gorm:"type:timestamp;not null;index:idx_events_start_end"`
-	EndAt       time.Time              `gorm:"type:timestamp;not null;index:idx_events_start_end"`
-	Timezone    string                 `gorm:"type:text;default:'UTC'"`
-	AllDay      bool                   `gorm:"type:boolean;default:false"`
-	Priority    int                    `gorm:"type:int;default:0"`
-	Color       string                 `gorm:"type:text"`
-	Meta        map[string]interface{} `gorm:"type:jsonb;default:'{}'"`
-	CreatedAt   time.Time              `gorm:"type:timestamp;default:now()"`
-	UpdatedAt   time.Time              `gorm:"type:timestamp;default:now()"`
-=======
 	"gorm.io/datatypes"
 )
 
@@ -42,5 +23,4 @@ type Event struct {
 	Meta        datatypes.JSON `gorm:"type:jsonb;default:'{}'::jsonb"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime"`
->>>>>>> 2ffebb8 ([FEAT][#6]: created gorm models for users, calendars, events and rrules, updated migrations)
 }
