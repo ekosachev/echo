99fbc588ac6ed4be5695175c4be9216dbdd2213e
Egor Kosachev <egorkosachev@vk.com>
2026-01-02 18:09:17 +0700

    added a popup with a calendar creation form

--- front/src/app/dashboard/components/CalendarList.tsx
+++ front/src/app/dashboard/components/CalendarList.tsx
@@ -1,25 +1,37 @@
 import { authFetch } from "@/lib/authFetch";
 import { CalendarCard } from "./CalendarCard";
 
-export default function CalendarList() {
+export const colorVariants = {
+  red: 'text-red-500',
+  orange: 'text-orange-500',
+  amber: 'text-amber-500',
+  yellow: 'text-yellow-500',
+  lime: 'text-lime-500',
+  green: 'text-green-500',
+  emerald: 'text-emerald-500',
+  teal: 'text-teal-500',
+  cyan: 'text-cyan-500',
+  sky: 'text-sky-500',
+  blue: 'text-blue-500',
+  indigo: 'text-indigo-500',
+  violet: 'text-violet-500',
+  purple: 'text-purple-500',
+  fuchsia: 'text-fuchsia-500',
+  pink: 'text-pink-500',
+  rose: 'text-rose-500',
+}
+
+export default async function CalendarList() {
+  const res = await authFetch("/api/v1/calendars");
+  const calendars = await res!.json();
   return (
 
-    <ul style={{ listStyleType: "none" }} className="flex flex-row gap-2 overflow-scroll md:flex-col">
-      <li className="block">
-        <CalendarCard iconColor="text-emerald-300" calendarName="Calendar 1"></CalendarCard>
-      </li>
-
-
-      <li className="block">
-
-        <CalendarCard iconColor="text-fuchsia-300" calendarName="Calendar 2"></CalendarCard>
-      </li>
-
-
-      <li className="block">
-
-        <CalendarCard iconColor="text-amber-300" calendarName="Calendar 3"></CalendarCard>
-      </li>
+    <ul style={{ listStyleType: "none" }} className="flex flex-row gap-2 overflow-auto md:flex-col ">
+      {calendars.calendars.map((calendar: any) => (
+        <li key={calendar.id} className="block">
+          <CalendarCard  iconColor={colorVariants[calendar.color as keyof typeof colorVariants]} calendarName={calendar.name}></CalendarCard>
+        </li>
+      ))}
 
     </ul>
   )
